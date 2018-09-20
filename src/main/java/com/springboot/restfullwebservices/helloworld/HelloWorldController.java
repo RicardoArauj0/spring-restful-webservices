292d1466cfd7832d6198656be4797113805eacca
Ricardo Araujo <ricardofranc.araujo@gmail.com>
2018-09-19 21:43:12 -0300

    Internationalization

--- src/main/java/com/springboot/restfullwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/springboot/restfullwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.springboot.restfullwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController()
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     //@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
@@ -23,5 +32,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    // Path variable
+    @GetMapping(path = "/hello-world-intern")
+    public String helloWorldI18n(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
