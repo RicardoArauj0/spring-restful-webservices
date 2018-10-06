47749288b6d31716a35d8940d3cadeb6464ad5ee
ricardofranc.araujo@gmail.com <ricardofranc.araujo@gmail.com>
2018-10-06 16:37:01 -0300

    Versioning with header and content negotiation approach

--- src/main/java/com/springboot/restfullwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/springboot/restfullwebservices/versioning/PersonVersioningController.java
@@ -1,7 +1,10 @@
 package com.springboot.restfullwebservices.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 public class PersonVersioningController {
@@ -16,4 +19,33 @@ public class PersonVersioningController {
         return new PersonV2(new Name("John", "Smith"));
     }
 
+    @GetMapping(value = "/person/param", params = "version=1")
+    public PersonV1 paramV1(){
+        return new PersonV1("John Smith");
+    }
+
+    @GetMapping(value = "/person/param", params = "version=2")
+    public PersonV2 paramV2(){
+        return new PersonV2(new Name("John", "Smith"));
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 headerV1(){
+        return new PersonV1("John Smith");
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 headerV2(){
+        return new PersonV2(new Name("John", "Smith"));
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesV1(){
+        return new PersonV1("John Smith");
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesV2(){
+        return new PersonV2(new Name("John", "Smith"));
+    }
 }
